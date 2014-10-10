95b1f015ec3ac958ea9a989476c6320c066237ac
AlexLeung <alex.l.leung@gmail.com>
2014-10-10 10:48:52 -0700

    ubdated database config so all points to db2 instead pf db1

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
-			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'ibsurvey',
-			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'AlexLeung',
-			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'localpass',
+			'host'      => isset($_SERVER['DB2_HOST']) ? $_SERVER['DB2_HOST'] : 'localhost',
+			'database'  => isset($_SERVER['DB2_NAME']) ? $_SERVER['DB2_NAME'] : 'ibsurvey',
+			'username'  => isset($_SERVER['DB2_USER']) ? $_SERVER['DB2_USER'] : 'AlexLeung',
+			'password'  => isset($_SERVER['DB2_PASS']) ? $_SERVER['DB2_PASS'] : 'localpass',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
